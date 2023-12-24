bbd7b7813d1e9bdea14644a1fda10b2f94ded411
JarmanjitSingh <jarmanjits176@gmail.com>
2023-12-24 23:18:09 +0530

    useContext and useReducer hook with ts

--- Typescript Projects/React With Ts Basics/src/App.tsx
+++ Typescript Projects/React With Ts Basics/src/App.tsx
@@ -1,60 +1,162 @@
-import { FormEvent, useState } from "react";
-import Box, { GenericComponent } from "./components/Box";
+// import { FormEvent, useState } from "react";
+// import Box, { GenericComponent } from "./components/Box";
 
-interface Person {
-  name: string;
-  age: number;
-}
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
+// function App() {
+//   const [val, setVal] = useState<string>("");
+//   const [user, setUser] = useState<Person>();
+
+//   const func1 = (a: string) => {
+//     console.log(a);
+//   };
+
+//   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//   console.log(user)
+//   };
+
+//   return (
+//     <>
+//       <Box heading="Hello world" count={50} func1={func1}>
+//         <h3>This is children</h3>
+//       </Box>
+
+//       <GenericComponent label="Search" value={val} setter={setVal} />
+
+//       <form onSubmit={submitHandler}>
+//         <h1>State with interface</h1>
+//         <input
+//           type="number"
+//           placeholder="Age"
+//           value={user?.age || ""}
+//           onChange={(e) =>
+//             setUser((prev) => ({
+//               ...prev!, //not mtlb not undefined
+//               age: Number(e.target.value),
+//             }))
+//           }
+//         />
+
+//         <input
+//           type="text"
+//           placeholder="Name"
+//           value={user?.name || ""}
+//           onChange={(e) =>
+//             setUser((prev) => ({
+//               ...prev!, //not mtlb not undefined
+//               name: e.target.value,
+//             }))
+//           }
+//         />
+
+//         <button type="submit">Submit</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default App;
+
+////////////////////////////// use context hook  //////////////////////////////
+
+// import { ReactNode, createContext, useState } from "react";
+// import ThemeComp from "./components/ThemeComp";
+
+// type ThemeType = "light" | "dark";
+
+// interface ThemeContextType {
+//   theme: ThemeType;
+//   toggleTheme: () => void;
+// }
+
+// export const ThemeContext = createContext<ThemeContextType>({
+//   theme: "light",
+//   toggleTheme: () => {},
+// });
+
+// const ThemeProvider = ({ children }: { children: ReactNode }) => {
+//   const [theme, setTheme] = useState<ThemeType>("light")
+
+//   const toggleTheme = () =>{
+//     setTheme(prev=> prev === "dark" ? "light" : "dark")
+//   }
+//   return (
+//     <ThemeContext.Provider value={{ theme, toggleTheme }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// };
+
+// function App() {
+//   return (
+//     <>
+//       <ThemeProvider>
+//        <ThemeComp />
+//       </ThemeProvider>
+//     </>
+//   );
+// }
+
+// export default App;
+
+///////////////////////////////// useReducer hook //////////////////////////////////
+import { useReducer } from "react";
+
+type StateType = {
+  count: number;
+};
+
+type ActionType =
+  | {
+      type: "Increment";
+      payload: number;
+    }
+  | {
+      type: "Decrement";
+      payload: number;
+    };
+
+const initialState: StateType = {
+  count: 0,
+};
+
+const reducer = (state: StateType, action: ActionType): StateType => {
+  switch (action.type) {
+    case "Increment":
+      return { count: state.count + action.payload };
+    case "Decrement":
+      return { count: state.count - action.payload };
+
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [val, setVal] = useState<string>("");
-  const [user, setUser] = useState<Person>();
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const func1 = (a: string) => {
-    console.log(a);
-  };
+  const increment = ():void=>{
+    dispatch({type: "Increment", payload: 1})
+  }
 
-  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  console.log(user)
-  };
+  const decrement = ():void=>{
+    dispatch({type: "Decrement", payload: 1})
+  }
+
 
   return (
     <>
-      <Box heading="Hello world" count={50} func1={func1}>
-        <h3>This is children</h3>
-      </Box>
+      <div>
+        <h1>Count Change</h1>
 
-      <GenericComponent label="Search" value={val} setter={setVal} />
-
-      <form onSubmit={submitHandler}>
-        <h1>State with interface</h1>
-        <input
-          type="number"
-          placeholder="Age"
-          value={user?.age || ""}
-          onChange={(e) =>
-            setUser((prev) => ({
-              ...prev!, //not mtlb not undefined
-              age: Number(e.target.value),
-            }))
-          }
-        />
-
-        <input
-          type="text"
-          placeholder="Name"
-          value={user?.name || ""}
-          onChange={(e) =>
-            setUser((prev) => ({
-              ...prev!, //not mtlb not undefined
-              name: e.target.value,
-            }))
-          }
-        />
-
-        <button type="submit">Submit</button>
-      </form>
+        <p>Count: {state.count}</p>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
     </>
   );
 }
---
+++ Typescript Projects/React With Ts Basics/src/components/ThemeComp.tsx
@@ -0,0 +1,15 @@
+// import { useContext } from "react";
+// import { ThemeContext } from "../App";
+
+// const ThemeComp = () => {
+//   const { theme, toggleTheme } = useContext(ThemeContext);
+//   return (
+//     <div className="boxContainer">
+//       <h1>Box 1</h1>
+//       <h3>current theme: {theme}</h3>
+//       <button onClick={toggleTheme}>Change Theme</button>
+//     </div>
+//   );
+// };
+
+// export default ThemeComp;
